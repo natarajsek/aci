9cedb130f31892eeb7bb0e298c589f3da7a77494
K201590 <K201590@NWMDWAM6923211.or.kp.org.>
2024-02-10 18:02:58 -0800

    merge to  local

---
+++ src/main/java/com/poc/app/git_poc/App.java
@@ -0,0 +1,18 @@
+package com.poc.app.git_poc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public void addData (String data){
+		
+	}
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
